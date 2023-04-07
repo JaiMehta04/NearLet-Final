758f1815d17ce4c763c35128d32490ebcdc13b70
Mjoshi8844 <100766853+Mjoshi8844@users.noreply.github.com>
2023-04-07 12:26:44 +0530

    Update indian_food.dart

--- lib/models/indian_food.dart
+++ lib/models/indian_food.dart
@@ -9,12 +9,12 @@ class IndianFood {
 
   static List<IndianFood> getLocalShops() {
     return const [
-      IndianFood(image: 'assets/images/food3.jpg', name: 'South\nIndian'),
-      IndianFood(image: 'assets/images/food5.jpg', name: 'Indian\nChai'),
-      IndianFood(image: 'assets/images/food1.jpg', name: 'North \nIndian'),
-      IndianFood(image: 'assets/images/food8.jpg', name: 'Indian\nBiryani'),
-      IndianFood(image: 'assets/images/food9.jpg', name: 'Indian\nDosa'),
-      IndianFood(image: 'assets/images/food4.jpg', name: 'Indian\nIdly'),
+      IndianFood(image: 'assets/images/food3.jpg', name: 'Grocery'),
+      IndianFood(image: 'assets/images/food5.jpg', name: 'Fruits\nVeggies'),
+      IndianFood(image: 'assets/images/food1.jpg', name: 'Mall'),
+      IndianFood(image: 'assets/images/food8.jpg', name: 'Stationary'),
+      IndianFood(image: 'assets/images/food9.jpg', name: 'Bookstore'),
+      IndianFood(image: 'assets/images/food4.jpg', name: 'Fruits'),
     ];
   }
 }
